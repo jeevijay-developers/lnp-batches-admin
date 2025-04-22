8b7d5c5504cabf37f55b87c30731a0cbdae80ccc
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-04-22 17:43:30 +0530

    coupons added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-'use client'
-import React, { useState } from 'react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+"use client";
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,16 +11,34 @@ const Navbar = () => {
         <div className="flex justify-between h-16 items-center">
           {/* Logo Section */}
           <div className="w-[200px] flex items-center">
-            <img className='w-full' src="/logo.png" alt="LnP-logo" />
+            <img className="w-full" src="/logo.png" alt="LnP-logo" />
           </div>
 
           {/* Desktop Menu */}
           <div className="hidden justify-end md:flex md:items-center md:space-x-6">
-            <button onClick={() => { window.location.href = '/home/todays-query' }} className="rounded-md font-semibold hover:cursor-pointer hover:text-blue-800 text-blue-500 transition">
+            <button
+              onClick={() => {
+                window.location.href = "/home/todays-query";
+              }}
+              className="rounded-md font-semibold hover:cursor-pointer hover:text-blue-800 text-blue-500 transition"
+            >
               Today's Query
             </button>
-            <button onClick={() => { window.location.href = '/home/all-time-query' }} className="rounded-md font-semibold hover:cursor-pointer hover:text-blue-800 text-blue-500 transition">
+            <button
+              onClick={() => {
+                window.location.href = "/home/all-time-query";
+              }}
+              className="rounded-md font-semibold hover:cursor-pointer hover:text-blue-800 text-blue-500 transition"
+            >
               All Time Queries
+            </button>
+            <button
+              onClick={() => {
+                window.location.href = "/coupons";
+              }}
+              className="rounded-md font-semibold hover:cursor-pointer hover:text-blue-800 text-blue-500 transition"
+            >
+              All Time Coupons
             </button>
             <button className="px-4 py-2 rounded-md font-semibold text-white bg-red-500 hover:bg-red-600 transition">
               Logout
@@ -49,10 +67,20 @@ const Navbar = () => {
       {isMenuOpen && (
         <div className="md:hidden bg-gray-900 border-t border-gray-800">
           <div className="mx-autopx-2 pt-2 pb-3 space-y-2 flex flex-col justify-center items-center">
-            <button onClick={() => { window.location.href = '/home/todays-query' }} className="w-1/2 px-4 py-2 rounded-md font-semibold text-white bg-blue-500 hover:bg-blue-600 transition text-center">
+            <button
+              onClick={() => {
+                window.location.href = "/home/todays-query";
+              }}
+              className="w-1/2 px-4 py-2 rounded-md font-semibold text-white bg-blue-500 hover:bg-blue-600 transition text-center"
+            >
               Today's Query
             </button>
-            <button onClick={() => { window.location.href = '/home/all-time-query' }} className="w-1/2 px-4 py-2 rounded-md font-semibold text-white bg-blue-500 hover:bg-blue-600 transition text-center">
+            <button
+              onClick={() => {
+                window.location.href = "/home/all-time-query";
+              }}
+              className="w-1/2 px-4 py-2 rounded-md font-semibold text-white bg-blue-500 hover:bg-blue-600 transition text-center"
+            >
               All Time Query
             </button>
             <button className="w-1/2 px-4 py-2 rounded-md font-semibold text-white bg-red-500 hover:bg-red-600 transition text-center">
